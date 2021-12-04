59d8f3fbf406451da3b904a99e0e6ea07bc3b3b9
Rolf1e <tigran.slama@laposte.net>
2021-12-04 13:05:28 +0100

    [AOC-2021] Day 1 - Part two

--- aoc-2021/src/day_one.rs
+++ aoc-2021/src/day_one.rs
@@ -25,6 +25,28 @@ fn count_larger(measurements: Vec<i32>) -> i16 {
     }
 }
 
+fn count_larger_2(measurements: Vec<i32>) -> i16 {
+    let size = measurements.len();
+    let mut sumed_by_three = Vec::with_capacity(size);
+
+    let mut first = 0;
+    let mut second = 1;
+    let mut third = 2;
+
+    loop {
+        if third == size {
+            return count_larger(sumed_by_three);
+        }
+
+        sumed_by_three.push(measurements[first] + measurements[second] + measurements[third]);
+
+        first += 1;
+        second += 1;
+        third += 1;
+
+    }
+}
+
 fn parse_file(file_name: &str) -> Vec<i32> {
     match read_aoc_input(file_name) {
         Ok(content) => content
@@ -48,7 +70,7 @@ fn string_to_i32(s: &String) -> i32 {
 /// https://adventofcode.com/2021/day/1
 #[cfg(test)]
 mod tests {
-    use crate::day_one::{count_larger, parse_file};
+    use crate::day_one::{count_larger, count_larger_2, parse_file};
     use std::path::PathBuf;
 
     // Part one
@@ -65,6 +87,23 @@ mod tests {
         dir.push(file_name);
 
         let measurements = parse_file(file_name);
-        assert_eq!(0, count_larger(measurements));
+        assert_eq!(1167, count_larger(measurements));
+    }
+
+    // Part two
+
+    #[test]
+    fn should_have_five_larger() {
+        let result = count_larger_2(vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263]);
+        assert_eq!(5, result)
+    }
+    #[test]
+    fn should_make_second_input() {
+        let file_name = "inputs/2021/day1/input2.txt";
+        let mut dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        dir.push(file_name);
+
+        let measurements = parse_file(file_name);
+        assert_eq!(1167, count_larger_2(measurements));
     }
 }
