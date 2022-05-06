be45c5ce252ec67b60619aa821242b0c08d53cac
Rolf1e <tigran.slama@laposte.net>
2022-05-06 22:27:54 +0200

    [2015] Day 4 - Part 1

---
+++ aoc-2015/src/day_four.rs
@@ -0,0 +1,46 @@
+// https://adventofcode.com/2015/day/4
+
+use md5::{Digest, Md5};
+use regex::Regex;
+
+/// Part one:
+/// input + numbers => to md5
+/// find first result starting by fives zeros
+pub fn find_md5(input: &str) -> i32 {
+    let regex =
+        Regex::new(r"^0{5,}.*").unwrap_or_else(|e| panic!("Failed to compile regex. error {}", e));
+    let mut payload = 0;
+    dbg!("HERE");
+    loop {
+        let current_attempt = format!("{}{}", input, payload.to_string());
+        let md5_attempt = format!("{:x}", Md5::digest(current_attempt));
+        println!("try {}: {}", payload, md5_attempt);
+        if regex.is_match(&md5_attempt) {
+            return payload;
+        } else {
+            payload += 1;
+        }
+    }
+}
+
+#[cfg(test)]
+pub mod test_2015_day_four {
+    use crate::day_four::find_md5;
+
+    // === Part one ===
+    // #[test]
+    pub fn should_be_609043() {
+        assert_eq!(609043, find_md5("abcdef"));
+    }
+
+    // #[test]
+    pub fn should_be_1048970() {
+        assert_eq!(1048970, find_md5("pqrstuv"));
+    }
+    
+    // #[test]
+    pub fn should_treat_input_1() {
+        assert_eq!(346386, find_md5("iwrupvqb"));
+    }
+
+}
---
+++ aoc-2015/src/main.rs
@@ -0,0 +1,8 @@
+use aoc_2015::day_four::find_md5;
+
+
+pub fn main() {
+    println!("hello");
+    let a = find_md5("abcdef");
+    println!("{}", a);
+}
