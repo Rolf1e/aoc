4489715926198bf5a58112f816c1c19b4725cc2e
Rolf1e <tigran.slama@contentsquare.com>
2022-12-08 13:23:50 +0100

    [2022] - Day 7 - part 1

---
+++ aoc2022/src/DaySeven.scala
@@ -0,0 +1,120 @@
+import aoccore.Day
+
+import scala.collection.mutable
+
+object DaySeven extends Day {
+  override def number: Int = 7
+
+  override def fileName: String = "inputs/dayseven.txt"
+
+  private val cwd = mutable.Stack("/")
+
+  override def partOne(): Unit = {
+    val lines = loadFromResource()
+    val fileSystem = FileSystem()
+    for (line <- lines) {
+
+      line match {
+        case s"$$ cd $dir" => {
+          if (dir == "..") cwd.pop()
+          else if (dir != "/") cwd.push(dir)
+        }
+        case s"$$ ls" => {}
+        case s"dir $dir" => {
+          val fullPath = pwd + s"/$dir"
+          fileSystem.mkdir(fullPath)
+        }
+        case s"$size $filename" => {
+          fileSystem.touch(pwd, File(filename, size.toInt))
+        }
+      }
+      //      fileSystem.ls()
+    }
+
+    val threshold = 100000
+    val directories = fileSystem.findSubdirWithAtMost(threshold)
+    println(directories.map(_.size).sum)
+  }
+
+  private def pwd: String = cwd.reverse.mkString("/")
+
+
+  override def partTwo(): Unit = ???
+
+}
+
+sealed trait Node {
+
+  def name: String
+
+  def size: Int
+
+  var children: Map[String, Node]
+}
+
+case class File(name: String, size: Int) extends Node {
+  override var children: Map[String, Node] = Map.empty
+}
+
+
+case class Dir(name: String, var children: Map[String, Node] = Map.empty) extends Node {
+  override def size: Int = children
+    .map { case (_, node) => node.size }
+    .sum
+}
+
+class FileSystem private(root: Node) {
+
+  def findSubdirWithAtMost(sizeThreshold: Int, node: Node = root): Seq[Node] = {
+    node.children
+      .view
+      .flatMap { case (_, node) =>
+        findSubdirWithAtMost(sizeThreshold, node) ++ (if (node.size < sizeThreshold) Seq(node) else Seq())
+      }
+      .filter(node => node match {
+        case File(_, _) => false
+        case Dir(_, _) => true
+      })
+      .toSeq
+  }
+
+  def ls(): Unit = {
+    println(root)
+  }
+
+  def mkdir(fullPath: String): Unit = {
+    var current = root
+    for (dir <- fullPath.split("/") if dir.nonEmpty) {
+      current = current.children.get(dir) match {
+        case Some(v) => v
+        case None => {
+          val newDir = Dir(dir)
+          current.children = current.children + (dir -> newDir)
+          newDir
+        }
+      }
+
+    }
+  }
+
+  def touch(fullPath: String, file: File): Unit = {
+    var current = root
+    if (fullPath != "/") {
+      for (dir <- fullPath.split("/") if dir.nonEmpty) {
+        current.children.get(dir) match {
+          case Some(value) => current = value
+          case None => throw new IllegalArgumentException(s"Can not touch $fullPath $file")
+        }
+      }
+    }
+    current.children = current.children + (file.name -> File(file.name, file.size))
+
+  }
+
+}
+
+object FileSystem {
+  def apply(): FileSystem = {
+    new FileSystem(Dir("/"))
+  }
+}
--- aoc2022/src/Main2022.scala
+++ aoc2022/src/Main2022.scala
@@ -1,7 +1,7 @@
 import aoccore.Day
 
 object Main2022 extends App {
-  private val days = Seq[Day](DayOne, DayTwo, DayThree, DayFour, DayFive, DaySix)
+  private val days = Seq[Day](DayOne, DayTwo, DayThree, DayFour, DayFive, DaySix, DaySeven)
   println("======= 2022 ========")
   Day.run(days)
 }
