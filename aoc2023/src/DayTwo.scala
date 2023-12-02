24909aa0988fcf2bb7b9d5d9a7318fc44dde812c
Rolf1e <tigran.slama@contentsquare.com>
2023-12-02 12:11:02 +0100

    feat(2023) - Day 2 - part 1 & 2

--- aoc2023/src/DayTwo.scala
+++ aoc2023/src/DayTwo.scala
@@ -5,8 +5,26 @@ object DayTwo extends Day {
 
   override def fileName: String = "inputs/daytwo.txt"
 
-  def main(args: Array[String]): Unit = {
-    DayTwo.partTwo()
+  case class Color(r: Int, g: Int, b: Int) {
+    def product: Int = r * g * b
+  }
+
+  def parseGame(line: String): (Int, Seq[Seq[Color]]) = {
+    line match {
+      case s"Game $id:$rest" => {
+        val sets = rest.split(";")
+          .map(set => {
+            set.split(",")
+              .map {
+                case s" $n green" => Color(0, n.toInt, 0)
+                case s" $n red" => Color(n.toInt, 0, 0)
+                case s" $n blue" => Color(0, 0, n.toInt)
+              }.toSeq
+          })
+
+        (id.toInt, sets.toSeq)
+      }
+    }
   }
 
 
@@ -18,70 +36,34 @@ object DayTwo extends Day {
 
     val sum = lines
       .map(line => parseGame(line))
-      .filter(_.isValid(maxRed, maxGreen, maxBlue))
-      .map(_.id)
+      .map { case (id, colors) => (id, colors.flatten) }
+      .filter { case (_, colors) => colors.forall(c => c.r <= maxRed && c.g <= maxGreen && c.b <= maxBlue) }
+      .map(_._1)
       .sum
 
     println(sum)
 
+  }
+
+  def maxColor(colors: Seq[Color]): Color = {
+    var r, g, b = 0
+    for (color <- colors) {
+      r = r.max(color.r)
+      b = b.max(color.b)
+      g = g.max(color.g)
+    }
+    Color(r, g, b)
   }
 
   override def partTwo(): Unit = {
     val lines = loadFromResource()
 
     val sum = lines
-      .view
-      .map(line => parseGame(line).sets)
-      .map(sets => sets.flatMap(s => s.draws).groupBy(_.color))
-      .map(game => game.map { case (_, draws) => draws.map(_.n).max })
-      .map(l => l.product)
+      .map(line => parseGame(line))
+      .map { case (_, colors) => maxColor(colors.flatten).product }
       .sum
 
     println(sum)
   }
-
-  def parseGame(line: String): Game = {
-    line match {
-      case s"Game $id:$rest" => {
-        val sets = rest.split(";")
-          .map(set => {
-            val draws = set.split(",")
-              .map {
-                case s" $n green" => Draw(n.toInt, "green")
-                case s" $n red" => Draw(n.toInt, "red")
-                case s" $n blue" => Draw(n.toInt, "blue")
-              }
-
-            Set(draws)
-          })
-
-        Game(id.toInt, sets)
-      }
-    }
-
-
-  }
-
-  case class Game(id: Int, sets: Seq[Set]) {
-    def isValid(red: Int, green: Int, blue: Int): Boolean =
-      sets.forall(_.isValid(red, green, blue))
-  }
-
-  case class Set(draws: Seq[Draw]) {
-    def isValid(red: Int, green: Int, blue: Int): Boolean =
-      draws.forall(_.isValid(red, green, blue))
-  }
-
-  case class Draw(n: Int, color: String) {
-
-    def isValid(red: Int, green: Int, blue: Int): Boolean = {
-      color match {
-        case "red" => n <= red
-        case "blue" => n <= blue
-        case "green" => n <= green
-      }
-    }
-  }
-
 
 }
